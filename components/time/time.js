4e2b7fc61bc0fb8a80b2e35da731cacf22a579ee
kano233333 <1320395666@qq.com>
2019-04-07 16:07:50 +0800

    first commit

---
+++ components/time/time.js
@@ -0,0 +1,20 @@
+Component({
+  data: {},
+  properties: {
+    fields: {
+      type: String,
+      value: 'month'
+    },
+    start: {
+      type: String,
+    },
+    end: {
+      type: String
+    }
+  },
+  methods: {
+    bindDateChange: function (e) {
+      console.log(e)
+    }
+  }
+});
