0d58b43e7f5f8dab6769f5fd9b53e5a576583135
kano233333 <1320395666@qq.com>
2019-04-20 20:40:04 +0800

    login_2

--- app.js
+++ app.js
@@ -9,14 +9,91 @@ App({
     ip:'http://192.168.1.70:8080/wx',
     intervalTime:60000
   },
-  requestFail:function(){
+  requestFail:function(xxx){
+    console.log(xxx)
     wx.showToast({
       title: '错误',
       duration: 1000,
       icon:"none"
     })
   },
-  aaa:function(a){
-    console.log(a)
+  isSq(){
+    var _this = this;
+    wx.getSetting({
+      success(res){
+        if(res.authSetting['scope.userInfo']){
+          _this.isLogin();
+        }
+      }
+    })
+  },
+  isLogin() {
+    var sessionID = wx.getStorageSync('sessionID');
+    console.log(sessionID)
+    var timestamp = wx.getStorageSync('timestamp');
+    var currenstamp = Date.parse(new Date());
+    console.log(currenstamp - timestamp)
+    var _this = this;
+
+    if (sessionID && (currenstamp - timestamp) < this.globalData.intervalTime) {
+      console.log('未过期')
+      wx.request({
+        url: this.globalData.ip + '/auth',
+        header: {
+          cookie: "JSESSIONID=" + sessionID + ";domain=localhost;path=/wx"
+        },
+        success: function (res) {
+          console.log(res)
+          var data = res.data.replace(/'/g, '"');
+          var _data = JSON.parse(data);
+          if (_data.static == 1) {
+            _this.getUserInfo();
+          }
+        }
+      })
+    } else if (sessionID == '' || (currenstamp - timestamp) >= this.globalData.intervalTime) {
+      console.log('已过期')
+      this.doLogin();
+    }
+  }
+  ,
+  getUserInfo(){
+    var _this = this;
+    wx.getUserInfo({
+      success(res){
+        _this.globalData.userInfo = res.userInfo;
+        console.log(res)
+      }
+    })
+  },
+  doLogin(){
+    var _this = this;
+    console.log('login')
+    wx.login({
+      success:function(res){
+        if(res.code){
+          wx.request({
+            url:_this.globalData.ip+'/auth',
+            data:{
+              code:res.code
+            },
+            success:function(res){
+              console.log(res)
+              var data = res.data.replace(/'/g,'"');
+              var _data = JSON.parse(data);
+              if(_data.static==1){
+                console.log(res.cookies[0].value)
+                wx.setStorageSync('sessionID', res.cookies[0].value);
+                var timestamp = Date.parse(new Date())
+                wx.setStorageSync('timestamp',timestamp)
+                _this.getUserInfo();
+              }else{
+                _this.requestFail()
+              }
+            }
+          })
+        }
+      }
+    })
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -8,100 +8,12 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    islogin:true
+    islogin:false
   },
   onLoad: function () {
-    this.isSq();
+    app.isSq();
   },
   getPickerTimer(data){
     console.log(data)
-  },
-  isSq(){
-    var _this = this;
-    wx.getSetting({
-      success(res){
-        if(res.authSetting['scope.userInfo']){
-          _this.isLogin();
-        } else {
-          _this.setData({
-            islogin:false
-          })
-        }
-      }
-    })
-  },
-  isLogin(){
-    var sessionID = wx.getStorageSync('sessionID');
-    console.log(sessionID)
-    var timestamp = wx.getStorageSync('timestamp');
-    var currenstamp = Date.parse(new Date());
-    console.log(currenstamp-timestamp)
-    var _this = this;
-
-    if(sessionID && (currenstamp-timestamp)<app.globalData.intervalTime){
-      console.log('未过期')
-      wx.request({
-        url:app.globalData.ip+'/auth',
-        header:{
-          cookie: "JSESSIONID="+sessionID+";domain=localhost;path=/wx"
-        },
-        success:function(res){
-          console.log(res)
-          var data = res.data.replace(/'/g,'"');
-          var _data = JSON.parse(data);
-          if(_data.static==1){
-            _this.getUserInfo();
-          }
-        }
-      })
-    }else if((currenstamp-timestamp)>=app.globalData.intervalTime){
-      console.log('已过期')
-      this.doLogin();
-    }else if(sessionID==''){
-      this.doLogin();
-    }
-  },
-  getUserInfo(){
-    var _this = this;
-    wx.getUserInfo({
-      success(res){
-        app.globalData.userInfo = res.userInfo;
-        console.log(res)
-        _this.setData({
-          islogin:true,
-          motto:'Hello '+app.globalData.userInfo.nickName
-        })
-      }
-    })
-  },
-  doLogin(){
-    var _this = this;
-    console.log('login')
-    wx.login({
-      success:function(res){
-        if(res.code){
-          wx.request({
-            url:app.globalData.ip+'/auth',
-            data:{
-              code:res.code
-            },
-            success:function(res){
-              console.log(res)
-              var data = res.data.replace(/'/g,'"');
-              var _data = JSON.parse(data);
-              if(_data.static==1){
-                console.log(res.cookies[0].value)
-                wx.setStorageSync('sessionID', res.cookies[0].value);
-                var timestamp = Date.parse(new Date())
-                wx.setStorageSync('timestamp',timestamp)
-                _this.getUserInfo();
-              }else{
-                app.requestFail()
-              }
-            }
-          })
-        }
-      }
-    })
   }
 })
